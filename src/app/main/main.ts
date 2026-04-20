0034d28249a4e383e43185e3c1f49dc1abcf4b50
André Rodrigues <oandredev@gmail.com>
2026-04-20 19:10:01 -0300

    fix: ajuste do css do header, footer, home e listagem

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
-export class Main {
-
-}
+export class Main {}
