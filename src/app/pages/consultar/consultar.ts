562ea956f8ab4aa7eab0643a0e85b242a85a201f
souzafe13 <souzafe13@gmail.com>
2026-04-21 14:36:54 -0300

    feat: implementa tela de consulta de alunos por ID

--- src/app/pages/consultar/consultar.ts
+++ src/app/pages/consultar/consultar.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AlunosService } from '../../core/services/alunos';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-consultar',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './consultar.html',
-  styleUrl: './consultar.css',
+  styleUrl: './consultar.css'
 })
 export class Consultar {
+  idBusca: string = '';
+  aluno: any = null; 
 
+  constructor(private alunosService: AlunosService) {}
+
+  validarSomenteNumeros() {
+    this.idBusca = this.idBusca.replace(/\D/g, '');
+  }
+
+  pesquisar() {
+    if (!this.idBusca) {
+      alert('Por favor, digite um ID.');
+      return;
+    }
+
+    this.alunosService.buscarPorId(this.idBusca).subscribe({
+      next: (dados) => {
+        this.aluno = dados;
+      },
+      error: () => {
+        alert('Aluno não encontrado!');
+        this.aluno = null;
+      }
+    });
+  }
 }
